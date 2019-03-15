92c6a13b0355f6ad13d9caaa3cf5fac26e6f23e6
saija jokela <saija.jokela@tuni.fi>
2019-03-15 14:08:17 +0200

    datan näyttö korjattu

--- OHSIHA_app/views.py
+++ OHSIHA_app/views.py
@@ -4,13 +4,11 @@ from OHSIHA_app.models import *
 from django.shortcuts import render
 import requests
 
-def home(request):
-    return render(request, "home.html"),
+def home():
+    return render("home.html")
 
-def index(request):
-
-    response = requests.get('http://api.teosto.fi/2017/event')
+def datasivu(request):
+    response = requests.get('http://api.teosto.fi/2017/event/')
     tapahtumat = response.json()
-    return render(request, 'etusivu.html', {
-        'tapahtumalista': tapahtumat['name']
-    })
+    return render(request, 'datasivu.html', {
+        'tapahtumalista': tapahtumat['events']})
--- ohsiha/urls.py
+++ ohsiha/urls.py
@@ -17,12 +17,11 @@ from django.contrib import admin
 from django.urls import path, include
 import OHSIHA_app.views
 from django.views.generic.base import TemplateView
-from django.urls import paths
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("home/", OHSIHA_app.views.home, name="home"),
+    path("datasivu/", OHSIHA_app.views.datasivu, name="datasivu"),
     path('accounts/', include('accounts.urls')),
     path('accounts/', include('django.contrib.auth.urls')),
     path('', TemplateView.as_view(template_name='home.html'), name='home'),
